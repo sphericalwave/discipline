6c848f477d4744ce0b4943145e19d3e9842b79a7
aaronium112 <aaron@sphericalwave.com>
2022-12-02 11:59:04 -0400

    initial icon and tabView

--- habit/habitApp.swift
+++ habit/habitApp.swift
@@ -11,7 +11,57 @@ import SwiftUI
 struct habitApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TabView {
+                absHabits
+                    .tag(0)
+                    .tabItem {
+                        Label("habits", systemImage: "bolt.fill")
+                    }
+                
+                crtHabits
+                    .tag(1)
+                    .tabItem {
+                        Label("today", systemImage: "bolt.fill")
+                    }
+            }
+        }
+    }
+    
+    var absHabits: some View {
+        NavigationStack {
+            List {
+                Section(header: Text("dawn")) {
+                    Text("meditate")
+                    Text("2k run")
+                    Text("shower")
+                }
+                
+                Section(header: Text("dusk")) {
+                    Text("meditate")
+                    Text("journal")
+                    Text("sleep learning")
+                }
+            }
+            .navigationTitle("absHabits")
+        }
+    }
+    
+    var crtHabits: some View {
+        NavigationStack {
+            List {
+                Section(header: Text("dawn")) {
+                    Text("meditate")
+                    Text("2k run")
+                    Text("shower")
+                }
+                
+                Section(header: Text("dusk")) {
+                    Text("meditate")
+                    Text("journal")
+                    Text("sleep learning")
+                }
+            }
+            .navigationTitle("crtHabits")
         }
     }
 }
