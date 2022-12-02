4e7f4bfaad3e7eeea94a82c71d4c16e79fafa576
aaronium112 <aaron@sphericalwave.com>
2022-12-02 11:30:57 -0400

    Initial Commit

---
+++ habit/habitApp.swift
@@ -0,0 +1,17 @@
+//
+//  habitApp.swift
+//  habit
+//
+//  Created by Aaron Anthony on 2022-12-02.
+//
+
+import SwiftUI
+
+@main
+struct habitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
